5df8eee604810d37e071b6d8c01abda85248cfc2
Joaquín Tamargo Azpilicueta <ParaisoNoFiscal@MBP-de-Joaquin.home>
2020-11-10 16:12:18 +0100

    Usage and error message added when parameters are not specified

--- ARNeatSequences.sh
+++ ARNeatSequences.sh
@@ -1,2 +1,11 @@
 #! /bin/bash
 
+if [ $# -eq 0 ]
+then
+	echo "Usage: bash ARNeatSequences.sh <params>"
+	echo ""
+	echo "params: Input file including the parameters"
+	exit 
+fi
+
+echo "Hello world"
